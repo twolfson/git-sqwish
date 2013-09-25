276d075e6caf22b47b711f2be900d002fafb1d63
Todd Wolfson <todd@twolfson.com>
2013-09-24 20:39:22 -0700

    Outlining tests

---
+++ test/git-sqwish_test.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+# A branch ahead of master
+
+  # when sqwished
+
+    # prompts for a message
+
+    # has past commit messages in logs
+
+    # when given a message
+
+      # is on a `.squashed` branch
+
+      # has the given commit message
+
+      # has the changes from
+
+      # is squashed (one commit ahead of `master)

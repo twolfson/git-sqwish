5c658f0b6f2a1f561834c8e98d205b0a56d4a81e
Todd Wolfson <todd@twolfson.com>
2013-09-24 22:56:35 -0700

    Added tests for bad commands

--- test/git-sqwish_test.sh
+++ test/git-sqwish_test.sh
@@ -18,42 +18,48 @@ fixture_dir() {
 # A branch ahead of master
 fixture_dir 'branch-ahead'
 
-  # when sqwished with a message
-  $BIN_DIR/git-sqwish master "Commit message 1" > /dev/null
+  # # when sqwished with a message
+  # $BIN_DIR/git-sqwish master "Commit message 1" > /dev/null
 
-    # is on a `.squashed` branch
-    # TODO: Renamed to .sqwished
-    test "$(git symbolic-ref --short HEAD)" == "dev/update.message.squashed" || echo "\`git-sqwish\` does not move to $BRANCH.squashed" 1>&2
+  #   # is on a `.squashed` branch
+  #   # TODO: Renamed to .sqwished
+  #   test "$(git symbolic-ref --short HEAD)" == "dev/update.message.squashed" || echo "\`git-sqwish\` does not move to $BRANCH.squashed in message-ful command in message-ful command" 1>&2
 
-    # is squashed (one commit ahead of `master)
-    test "$(git log master..dev/update.message.squashed --format=oneline | wc -l)" == "1" || echo "\`git-sqwish\` did not squash commits to one commit" 1>&2
+  #   # is squashed (one commit ahead of `master)
+  #   test "$(git log master..dev/update.message.squashed --format=oneline | wc -l)" == "1" || echo "\`git-sqwish\` did not squash commits to one commit in message-ful command" 1>&2
 
-    # used the given message and previous commits in its commit
-    echo $(git log -n 1 --format=oneline)
-    # test "$(git log master..dev/update.message.squashed --format=oneline)" == " Commit message 1 Updated message.txt" || echo 'a'
+  #   # used the given message and previous commits in its commit
+  #   test "$(git log -n 1 --format=%s)" == "Commit message 1 Updated message.txt" || echo "\`git-sqwish\` does not contain provided or past commit messages in message-ful command" 1>&2
 
-    # has the changes from previous branch
+  #   # has the changes from previous branch
+  #   test "$(cat message.txt)" == "Goodbye Earth" || echo "\`git-sqwish\` did not copy changes from previous branch in message-ful command" 1>&2
 
-  # when sqwished without a message
-  # TODO: Fix this. https://github.com/twolfson/git-sqwish/issues/1
-  # expect $TEST_DIR/expect_tmp.sh 1> /dev/null
-  # cat message.txt
-  # git status --porcelain
+  # # when sqwished without a message
+  # # TODO: Fix this. https://github.com/twolfson/git-sqwish/issues/1
+  # # expect $TEST_DIR/expect_tmp.sh 1> /dev/null
+  # # cat message.txt
+  # # git status --porcelain
 
-    # prompts for a message
+  #   # prompts for a message
 
-    # when given a message
+  #   # when given a message
 
-      # has the message appended to previous logs
+  #     # has the message appended to previous logs
 
-      # has the changes from previous branch
+  #     # has the changes from previous branch
 
   # when sqwished without a master branch
+  fixture_dir 'branch-ahead'
+  OUTPUT="$($BIN_DIR/git-sqwish 2>&1)"
 
     # exits with a non-zero code
+    test "$?" != 0 || echo "$? == 0 for master-less command" 1>&2
 
     # informs user about missing parameter
+    test "$OUTPUT" == "Master branch required." || echo "\`git sqwish\` did not inform user of missing parameter for master-less command" 1>&2
 
+    # is on the original branch
+    test "$(git symbolic-ref --short HEAD)" == "dev/update.message" || echo "\`git-sqwish\` moved off of original branch in master-less command" 1>&2
 
 # A dirty branch
 

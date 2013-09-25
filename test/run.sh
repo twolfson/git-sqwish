09e3310c5c59c725fa11ef32779a5845cba64cc1
Todd Wolfson <todd@twolfson.com>
2013-09-24 23:29:24 -0700

    Added test runner from sexy-bash-prompt

---
+++ test/run.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+# Run the test suite
+./test/git-sqwish_test.sh 2> test.stderr
+
+# If there were test failures, fail
+STDERR=$(cat test.stderr)
+if test -n "$STDERR"; then
+  echo "# ERRORS OCCURRED. STDERR OUTPUT:" 1>&2
+  echo $STDERR 1>&2
+  exit 1
+else
+  echo "# All tests passed!"
+  exit 0
+fi
